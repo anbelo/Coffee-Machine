0cf41c3033587c2579b3a2633df8373f776a01e4
anbelo <andreij.belokurov@gmail.com>
2022-09-10 03:05:04 +0300

    Brush up your code (Stage 6/6)

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,103 +1,192 @@
 package machine
 
-const val ESPRESSO_WATER = 250
-const val ESPRESSO_COFFEE_BEANS = 16
-const val ESPRESSO_COST = 4
+class Machine {
 
-const val LATTE_WATER = 350
-const val LATTE_MILK = 75
-const val LATTE_COFFEE_BEANS = 20
-const val LATTE_COST = 7
+    private var water = 400
+    private var coffeeBeans = 120
+    private var milk = 540
+    private var money = 550
+    private var cup = 9
 
-const val CAPPUCCINO_WATER = 200
-const val CAPPUCCINO_MILK = 100
-const val CAPPUCCINO_COFFEE_BEANS = 12
-const val CAPPUCCINO_COST = 6
+    var isPowerOn = true
+        private set
 
-const val BUY = "buy"
-const val FILL = "fill"
-const val TAKE = "take"
-const val REMAINING = "remaining"
-const val EXIT = "exit"
+    private enum class Coffee(val water: Int, val beans: Int, val milk: Int, val cost: Int) {
+        ESPRESSO(250, 16, 0, 4),
+        LATTE(350, 20, 75, 7),
+        CAPPUCCINO(200, 12, 100, 6)
+    }
 
-var MACHINE_HAS_WATER = 400
-var MACHINE_HAS_MILK = 540
-var MACHINE_HAS_COFFEE_BEANS = 120
-var MACHINE_HAS_CUP = 9
-var MACHINE_HAS_MONEY = 550
+    private interface State {
+        fun prompt()
+        fun readInput(s: String)
+    }
 
-fun main() {
-    chooseAction()
-}
+    private val chooseAction = ChooseAction(this)
+    private val chooseCoffeeType = ChooseCoffeeType(this)
+    private val fillWater = FillWater(this)
+    private val fillMilk = FillMilk(this)
+    private val fillCoffeeBeans = FillCoffeeBeans(this)
+    private val fillCups = FillCups(this)
 
-fun state() {
-    println("""
-        The coffee machine has:
-        $MACHINE_HAS_WATER ml of water
-        $MACHINE_HAS_MILK ml of milk
-        $MACHINE_HAS_COFFEE_BEANS g of coffee beans
-        $MACHINE_HAS_CUP disposable cups
-        $$MACHINE_HAS_MONEY of money
-    """.trimIndent())
-}
+    private var currentState: State = chooseAction
 
-fun chooseAction() {
-    while (true) {
-        print("Write action ($BUY, $FILL, $TAKE. $REMAINING, $EXIT): ")
-        when (readln()) {
-            BUY -> buyCoffee()
-            FILL -> fillCoffeeMachine()
-            TAKE -> takeMoney()
-            REMAINING -> state()
-            EXIT -> break
-            else -> println("Unknown Action")
+    private class ChooseAction(val machine: Machine): State {
+        private val buy = "buy"
+        private val fill = "fill"
+        private val take = "take"
+        private val remaining = "remaining"
+        private val exit = "exit"
+
+        private fun takeMoney() {
+            println()
+            println("I gave you $${machine.money}")
+            println()
+            machine.money = 0
+        }
+
+        private fun remaining() {
+            println("""
+                
+                The coffee machine has:
+                ${machine.water} ml of water
+                ${machine.milk} ml of milk
+                ${machine.coffeeBeans} g of coffee beans
+                ${machine.cup} disposable cups
+                $${machine.money} of money
+                
+                """.trimIndent()
+            )
+        }
+
+        override fun prompt() {
+            println("Write action ($buy, $fill, $take, $remaining, $exit): ")
+        }
+
+        override fun readInput(s: String) {
+            when (s) {
+                buy -> {
+                    machine.currentState = machine.chooseCoffeeType
+                }
+                fill -> {
+                    machine.currentState = machine.fillWater
+                }
+                take -> {
+                    takeMoney()
+                    machine.currentState = machine.chooseAction
+                }
+                remaining -> {
+                    remaining()
+                    machine.currentState = machine.chooseAction
+                }
+                exit -> {
+                    machine.isPowerOn = false
+                }
+                else -> println("Unknown Action")
+            }
         }
     }
-}
 
-fun buyCoffee() {
-    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
-    when (readln()) {
-        "1" -> makeCoffee(ESPRESSO_WATER, 0, ESPRESSO_COFFEE_BEANS, ESPRESSO_COST)
-        "2" -> makeCoffee(LATTE_WATER, LATTE_MILK, LATTE_COFFEE_BEANS, LATTE_COST)
-        "3" -> makeCoffee(CAPPUCCINO_WATER, CAPPUCCINO_MILK, CAPPUCCINO_COFFEE_BEANS, CAPPUCCINO_COST)
-        "back" -> {} // ignore
-        else -> println("Unknown drink")
+    private class ChooseCoffeeType(val machine: Machine): State {
+        override fun prompt() {
+            println()
+            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ")
+        }
+
+        private fun makeCoffee(coffee: Coffee) {
+            if (machine.water < coffee.water) {
+                println("Sorry, not enough water!")
+            } else if (machine.milk < coffee.milk) {
+                println("Sorry, not enough milk!")
+            } else if (machine.coffeeBeans < coffee.beans) {
+                println("Sorry, not enough coffee beans!")
+            } else if (machine.cup <= 0) {
+                println("Sorry, not enough disposable cups!")
+            } else {
+                println("I have enough resources, making you a coffee!")
+                machine.water -= coffee.water
+                machine.milk -= coffee.milk
+                machine.coffeeBeans -= coffee.beans
+                machine.cup--
+                machine.money += coffee.cost
+            }
+        }
+
+        override fun readInput(s: String) {
+            when (s) {
+                "1" -> makeCoffee(Coffee.ESPRESSO)
+                "2" -> makeCoffee(Coffee.LATTE)
+                "3" -> makeCoffee(Coffee.CAPPUCCINO)
+                "back" -> {} // ignore
+                else -> println("Unknown drink")
+            }
+            println()
+            machine.currentState = machine.chooseAction
+        }
     }
-}
 
-fun makeCoffee(water: Int, milk: Int, coffeeBeans: Int, cost: Int) {
-    if (MACHINE_HAS_WATER < water) {
-        println("Sorry, not enough water!")
-    } else if (MACHINE_HAS_MILK < milk) {
-        println("Sorry, not enough milk!")
-    } else if (MACHINE_HAS_COFFEE_BEANS < coffeeBeans) {
-        println("Sorry, not enough coffee beans!")
-    } else if (MACHINE_HAS_CUP <= 0) {
-        println("Sorry, not enough disposable cups!")
-    } else {
-        println("I have enough resources, making you a coffee!")
-        MACHINE_HAS_WATER -= water
-        MACHINE_HAS_MILK -= milk
-        MACHINE_HAS_COFFEE_BEANS -= coffeeBeans
-        MACHINE_HAS_CUP--
-        MACHINE_HAS_MONEY += cost
+    private class FillWater(val machine: Machine): State {
+        override fun prompt() {
+            println()
+            println("Write how many ml of water do you want to add: ")
+        }
+
+        override fun readInput(s: String) {
+            machine.water += s.toInt()
+            machine.currentState = machine.fillMilk
+        }
     }
+
+    private class FillMilk(val machine: Machine): State {
+        override fun prompt() {
+            println("Write how many ml of milk do you want to add: ")
+        }
+
+        override fun readInput(s: String) {
+            machine.milk += s.toInt()
+            machine.currentState = machine.fillCoffeeBeans
+        }
+    }
+
+    private class FillCoffeeBeans(val machine: Machine): State {
+        override fun prompt() {
+            println("Write how many grams of coffee beans do you want to add: ")
+        }
+
+        override fun readInput(s: String) {
+            machine.coffeeBeans += s.toInt()
+            machine.currentState = machine.fillCups
+        }
+    }
+
+    private class FillCups(val machine: Machine): State {
+        override fun prompt() {
+            println("Write how many disposable cups of coffee do you want to add: ")
+        }
+
+        override fun readInput(s: String) {
+            machine.cup += s.toInt()
+            println()
+            machine.currentState = machine.chooseAction
+        }
+    }
+
+    fun prompt() {
+        currentState.prompt()
+    }
+
+    fun readInput(s: String) {
+        currentState.readInput(s)
+    }
+
 }
 
-fun fillCoffeeMachine() {
-    print("Write how many ml of water do you want to add: ")
-    MACHINE_HAS_WATER += readln().toInt()
-    print("Write how many ml of milk do you want to add: ")
-    MACHINE_HAS_MILK += readln().toInt()
-    print("Write how many grams of coffee beans do you want to add: ")
-    MACHINE_HAS_COFFEE_BEANS += readln().toInt()
-    print("Write how many disposable cups of coffee do you want to add: ")
-    MACHINE_HAS_CUP += readln().toInt()
-}
 
-fun takeMoney() {
-    println("I gave you $$MACHINE_HAS_MONEY")
-    MACHINE_HAS_MONEY = 0
+fun main() {
+    val machine = Machine()
+    do {
+        machine.prompt()
+        machine.readInput(readln())
+    } while (machine.isPowerOn)
 }
 
