13d81086e08279060e8512ab38ba52a17de7ea42
anbelo <andreij.belokurov@gmail.com>
2022-09-10 03:00:31 +0300

    Ingredient calculator (Stage 3/6)

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -11,12 +11,25 @@ fun main() {
         Coffee is ready!
     """.trimIndent())
 
+    print("Write how many ml of water the coffee machine has: ")
+    val waterAmount = readln().toInt()
+    print("Write how many ml of milk the coffee machine has: ")
+    val milkAmount = readln().toInt()
+    print("Write how many grams of coffee beans the coffee machine has: ")
+    val coffeeBeansAmount = readln().toInt()
     print("Write how many cups of coffee you will need: ")
     val cupCount = readln().toInt()
-    print("""
-        For $cupCount cups of coffee you will need:
-        ${cupCount * 200} ml of water
-        ${cupCount * 50} ml of milk
-        ${cupCount * 15} g of coffee beans
-    """.trimIndent())
+
+    val maxWaterCups = waterAmount / 200
+    val maxMilkCups = milkAmount / 50
+    val coffeeBeansCups = coffeeBeansAmount / 15
+    val maxCups = maxWaterCups.coerceAtMost(maxMilkCups).coerceAtMost(coffeeBeansCups)
+
+    if (cupCount < maxCups) {
+        print("Yes, I can make that amount of coffee (and even ${maxCups - 1} more than that)")
+    } else if (cupCount == maxCups) {
+        print("Yes, I can make that amount of coffee")
+    } else {
+        print("No, I can make only $maxCups cups of coffee")
+    }
 }
